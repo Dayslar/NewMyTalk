b803d7c365baea002da193bd80e696a308411da1
Dayslar <zalobe8564>
2016-07-01 11:13:32 +0300

    rename methods in interface a CurrentRecord

--- app/src/main/java/com/example/dayslar/newmytalk/database/interfaces/CurrentRecord.java
+++ app/src/main/java/com/example/dayslar/newmytalk/database/interfaces/CurrentRecord.java
@@ -2,8 +2,8 @@ package com.example.dayslar.newmytalk.database.interfaces;
 
 public interface CurrentRecord {
 
-    void updateInt(String column, int value);
-    void updateLong(String column, long value);
-    void updateString(String column, String value);
-    void updateBoolean(String column, boolean value);
+    void update(String column, int value);
+    void update(String column, long value);
+    void update(String column, String value);
+    void update(String column, boolean value);
 }
