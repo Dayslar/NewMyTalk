f507c6476cefc1be8f8ce9a88bfd09a6d87628d6
Dayslar <zalobe8564>
2016-08-31 13:33:03 +0300

    Добавил простую реализацию запроса разрешений

--- app/src/main/java/com/example/dayslar/newmytalk/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/dayslar/newmytalk/ui/activity/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.dayslar.newmytalk.ui.activity;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -14,6 +19,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.dayslar.newmytalk.R;
 import com.example.dayslar.newmytalk.db.entity.Manager;
@@ -36,6 +42,7 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @EActivity(R.layout.main_activity)
@@ -61,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @AfterViews
-    void init(){
+    void init() {
         this.context = this;
         this.managerDao = SqlManagerDao.getInstance(this);
         this.managerService = new NetworkManagerService(this);
@@ -70,7 +77,53 @@ public class MainActivity extends AppCompatActivity {
         initRecycleView();
         initToolbar();
         initFab();
+        initPermissions();
 
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // TODO: 29.08.2016  
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        boolean isActivePermission = true;
+        if (requestCode == 99) {
+
+            for (int grantResult : grantResults) {
+                if (grantResult == PackageManager.PERMISSION_DENIED)
+                    isActivePermission = false;
+            }
+
+            if (!isActivePermission) {
+                Toast.makeText(this, "Для корректной работы приложения неоходимы все разрешения!", Toast.LENGTH_SHORT).show();
+                initPermissions();
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void initPermissions() {
+
+        ArrayList<String> permissionsList = new ArrayList<>();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+            permissionsList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED)
+            permissionsList.add(Manifest.permission.CALL_PHONE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_DENIED)
+            permissionsList.add(Manifest.permission.READ_PHONE_STATE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED)
+            permissionsList.add(Manifest.permission.RECORD_AUDIO);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_DENIED)
+            permissionsList.add(Manifest.permission.READ_CONTACTS);
+
+
+        if (permissionsList.size() > 0)
+            ActivityCompat.requestPermissions(this, permissionsList.toArray(new String[]{}), 99);
     }
 
     private void getServerMangers() {
@@ -136,10 +189,7 @@ public class MainActivity extends AppCompatActivity {
         toolbar.inflateMenu(R.menu.main_menu);
     }
 
-    @Override
-    public void onBackPressed() {
-        // TODO: 29.08.2016  
-    }
+
 
     private void initRecycleView() {
         GridLayoutManager glm = new GridLayoutManager(this, 3);
